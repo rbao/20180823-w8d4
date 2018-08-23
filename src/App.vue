0d148d805addf6edc70d37900bdc19508b0c9346
Roy Bao <roy@roybao.com>
2018-08-23 12:12:32 -0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,88 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{title}}</h1>
+
+    <ul>
+      <url-item v-for="url in urlDatabase" :url="url" @edit="editUrl"></url-item>
+    </ul>
+
+    <form @submit.prevent="submit">
+      <input v-model="urlObject.shortUrl" type="text" name="short-url" placeholder="Enter a short url...">
+      <input v-model="urlObject.longUrl" type="text" name="long-url" placeholder="Enter a long url...">
+
+      <button type="submit">Submit</button>
+      <button v-if="editingId" @click="cancel" type="button">Cancel</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UrlItem from './components/url-item'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    UrlItem
+  },
+  data () {
+    return {
+      title: 'TinyApp',
+      urlObject: {
+        shortUrl: '',
+        longUrl: ''
+      },
+      editingId: '',
+      nextId: 3,
+      urlDatabase: [
+        { id: 1, shortUrl: 'gg', longUrl: 'http://google.ca' },
+        { id: 2, shortUrl: 'fb', longUrl: 'http://facebook.com' }
+      ]
+    }
+  },
+  methods: {
+    submit (e) {
+      if (!this.urlObject.shortUrl || !this.urlObject.longUrl) {
+        return
+      }
+
+      if (this.editingId) {
+        // 1. Find url we are currently editing
+        let targetUrlObject = this.urlDatabase.find((urlObject) => {
+          return urlObject.id === this.editingId
+        })
+
+        // 2. Update properties accordingly
+        targetUrlObject.shortUrl = this.urlObject.shortUrl
+        targetUrlObject.longUrl = this.urlObject.longUrl
+
+        this.cancel()
+      } else {
+        this.urlObject.id = this.nextId
+        this.urlDatabase.push(this.urlObject)
+        this.nextId = this.nextId + 1
+
+        this.cancel()
+      }
+    },
+
+    editUrl (urlId) {
+      this.editingId = urlId
+
+      // 1. Find the existing url object inside the database
+      let targetUrlObject = this.urlDatabase.find((urlObject) => {
+        return urlObject.id === urlId
+      })
+
+      // 2. Prefill the input with existing information
+      this.urlObject = Object.assign({}, targetUrlObject)
+    },
+
+    cancel () {
+      this.editingId = undefined
+
+      this.urlObject = { shortUrl: '', longUrl: '' }
+    }
   }
 }
 </script>
@@ -21,8 +92,11 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+button {
+  margin-left: 10px;
 }
 </style>
---
+++ src/components/url-item.vue
@@ -0,0 +1,29 @@
+<template>
+  <li>
+    <span>{{url.shortUrl}}</span> - <a :href="url.longUrl" target="_blank">{{url.longUrl}}</a>
+    <button @click="editClicked">Edit</button>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'UrlItem',
+  data () {
+    return {}
+  },
+  props: {
+    url: {
+      required: true,
+      type: Object
+    }
+  },
+  methods: {
+    editClicked () {
+      this.$emit('edit', this.url.id)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
